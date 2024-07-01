7ac0d37a6e052093bd0af5b204ec47160d5d72d5
Jordon Marchesano <jordonomarchesano@gmail.com>
2024-06-30 22:33:55 -0700

    done

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,11 @@ import React from "react";
 import AnimatedText from "./AnimatedText";
 import Image from "next/image";
 import mockUp from "../public/images/home.png";
+import { GithubIcon } from "./Icons";
 
 const Header = () => {
   return (
-    <div className="grid grid-cols-12 mt-24  xl:mx-[10rem]">
-      
+    <div className="grid grid-cols-12 mt-24  xl:mx-[10rem] ">
       <div className="lg:col-span-6 col-span-12 ">
         <AnimatedText
           className="!text-6xl lg:text-left md:text-left md:ml-10 xl:!text-7xl lg:!text-6xl md:!text-5xl  "
@@ -16,7 +16,14 @@ const Header = () => {
           className="!text-3xl lg:text-left md:text-left md:ml-10 xl:!text-4xl  lg:!text-4xl md:!text-4xl  "
           text="React Native Demo"
         />
+        <div className="flex justify-center md:ml-10 lg:justify-start md:justify-start">
+          <p className="text-dark font-mont ">by: Jordon Marchesano</p>
+          <a className="" href="https://github.com/cHoMpDiSs/IOS-Skate-Shop-React-Native  ">
+            <GithubIcon className={"w-10 bg-dark rounded-xl mt-[0.15rem] ml-1"} />
+          </a>
+        </div>
       </div>
+
       <div className="mx-auto lg:col-span-6 lg:-rotate-45 xl:ml-[10rem] lg:ml-[5rem] col-span-12 w-[10rem] mt-10">
         <div
           className="relative  w-full items-center  justify-between rounded-3xl rounded-br-3xl border
@@ -30,7 +37,7 @@ border-solid border-dark bg-light shadow-2xl  dark:border-light dark:bg-dark  lg
         xs:rounded-[1.5rem] "
           />
           <Image
-            className="rounded-[1.5rem] w-full bg-dark mb-2 ml-2 border border-dark"
+            className="rounded-[1.5rem] w-full bg-dark mb-2 ml-2 border border-dark hidden lg:block"
             src={mockUp}
           />
         </div>
--- components/Screens.js
+++ components/Screens.js
@@ -5,7 +5,7 @@ import cartPic from "../public/images/cart.png";
 import pantsPic from "../public/images/pants.png";
 // import demoVid from "../public/video/demo.mp4"
 import AnimatedText from "./AnimatedText";
-
+import mockUp from "../public/images/home.png";
 
 const Screen = ({ text, pic, className = "" }) => {
   return (
@@ -13,7 +13,7 @@ const Screen = ({ text, pic, className = "" }) => {
       <AnimatedText className={`ml-3  ${className}`} text={text} />
 
       <div
-        className="relative  w-[40%] mx-auto rounded-3xl rounded-br-3xl border
+        className="relative md:w-[12rem] sm:w-[10rem] w-[20rem] mx-auto rounded-3xl rounded-br-3xl border
 border-solid border-dark bg-light shadow-2xl  dark:border-light dark:bg-dark  lg:flex-col 
  xs:rounded-2xl  xs:rounded-br-3xl 
     "
@@ -32,47 +32,57 @@ border-solid border-dark bg-light shadow-2xl  dark:border-light dark:bg-dark  lg
   );
 };
 
-const VideoScreen = ({ text, vid, className = "" }) => {
-    return (
-      <>
-        <AnimatedText className={`ml-3  ${className}`} text={text} />
-  
-        <div
-          className="relative  w-[40%] mx-auto rounded-3xl rounded-br-3xl border
-  border-solid border-dark bg-light shadow-2xl  dark:border-light dark:bg-dark  lg:flex-col 
-   xs:rounded-2xl  xs:rounded-br-3xl 
-      "
-        >
-          <div
-            className="absolute  top-0 -right-3 -z-10 h-[103%] w-[101%] rounded-[2.5rem] rounded-br-3xl bg-dark
-           dark:bg-light  xs:-right-2 xs:h-[102%] xs:w-[100%]
-          xs:rounded-[1.5rem] "
-          />
-          <Image
-            className="rounded-[1.5rem] w-full  bg-dark mb-2 ml-2 border border-dark  "
-            src={pic}
-          />
-        </div>
-      </>
-    );
-  };
+const Video = () => {
+  return (
+    <div
+      className="relative  md:w-[12rem] sm:w-[10rem] w-[20rem]  mx-auto rounded-[1.5rem] rounded-br-[35px] rounded-bl-[35px]  rounded-tr-[34px] border
+      border-solid border-dark bg-light shadow-2xl  dark:border-light dark:bg-dark  lg:flex-col 
+       "
+    >
+      <div
+        className="absolute top-0 -right-3 -z-10 h-[103%] w-[100%] rounded-[2.5rem] rounded-br-3xl bg-dark
+         dark:bg-light  xs:-right-2 xs:h-[102%] xs:w-[100%]
+        xs:rounded-[1.5rem] "
+      />
+
+      <video
+        className="mt-[.rem] md:w-[12rem] sm:w-[9rem] w-[19rem]   ml-[.7rem] mb-2 rounded-r-[25px] rounded-l-[25px] rounded-[1.5rem] xs:rounded-br-2xl border border-dark"
+        poster={mockUp.src}
+        controls
+        preload="none"
+        autoPlay={true}
+      >
+        <source src="/video/demo.mp4" type="video/mp4" />
+        <track
+          src="/video/demo.vtt"
+          kind="subtitles"
+          srcLang="en"
+          label="English"
+        />
+        Your browser does not support the video tag.
+      </video>
+      {/* </div> */}
+    </div>
+  );
+};
 
 const Screens = () => {
   return (
-    <div className="flex flex-1 mx-auto mt-24 ">
+    <div className="flex flex-1 mx-auto mt-24 mb-48">
       <div className="   mt-10 justify-center items-center">
+        <Video />
         <Screen
-          className={"lg:w-[150%] mt-14"}
+          className={" mt-14"}
           text={"Deck Screen"}
           pic={boardsPic}
         />
         <Screen
-          className={"lg:w-[150%] mt-14"}
+          className={" mt-14"}
           text={"Pants Screen"}
           pic={pantsPic}
         />
         <Screen
-          className={"lg:w-[150%] mt-14"}
+          className={"mt-14"}
           text={"Cart Screen"}
           pic={cartPic}
         />
